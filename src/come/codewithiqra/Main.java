00d0038f5630b9ae0110712765d6582a86ce327d
HP 6th <iqra615>
2020-06-18 15:43:24 -0700

    Installed

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,14 @@
+package come.codewithiqra;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("/nJavaVersion: "+System.getProperty("java.version"));
+        System.out.println("RuntimeVersion: "+System.getProperty("RuntimeVersion"));
+        System.out.println("JavaHome: "+System.getProperty("JavaHome"));
+        System.out.println("JavaVendor: "+System.getProperty("JavaVendor"));
+        System.out.println("Java Class Path: "+System.getProperty("Java Class Path\n"));
+
+    }
+}
